83436a577e09db1e7334479a7cbe1552094eb4b0
Joel Aro <joelaro@outlook.com>
2015-11-03 01:09:10 -0500

    It works!

--- cli-discord.py
+++ cli-discord.py
@@ -1,12 +1,14 @@
 import discord
 import logging
 import sys
+import threading
 
 # Set up the logging module to output diagnostic to the console.
 logging.basicConfig()
 
 client = discord.Client()
 client.login(str(sys.argv[1]), str(sys.argv[2]))
+currchannel = 0
 
 if not client.is_logged_in:
     print('Logging in to Discord failed')
@@ -14,15 +16,28 @@ if not client.is_logged_in:
 
 @client.event
 def on_ready():
-    print('Connected!')
-    print('Username: ' + client.user.name)
-    print('ID: ' + client.user.id)
+    print('Connected as ' + client.user.name)
 
 @client.event
 def on_message(message):
     if message.author.id != client.user.id:
         print('[' + message.channel.name + '] '
         + message.author.name + ': ' + message.content)
+    if message.content.startswith('/ch'):
+        print message.channel.id
+        global currchannel
+        currchannel = message.channel
 
+def read_in():
+    while client.is_logged_in:
+        if currchannel:
+            client.send_message(currchannel, raw_input(currchannel.name + '>'))
+            print(raw_input(currchannel.name + '>'))
+
+t = threading.Thread(target=read_in)
+# Kills the thread when the program closes
+# Otherwise you would have to kill it manually
+t.daemon = True
+t.start()
 
 client.run()
